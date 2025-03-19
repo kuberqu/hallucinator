1f061d6797aa32568e715175649dbcc202995012
kuberqu <124673743+kuberqu@users.noreply.github.com>
2025-03-19 10:19:29 +0100

    Update torch.js
    pytorch Version changed to 126

--- torch.js
+++ torch.js
@@ -7,7 +7,7 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121"
+        "message": "pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126"
       }
     },
     // windows amd
